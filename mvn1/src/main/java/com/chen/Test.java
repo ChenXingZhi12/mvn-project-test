4054dbcdcfc23f368c5fa57a6cd159be32803632
ChenXingZhi12 <909716454@qq.com>
2023-10-09 09:51:54 +0800

    第一次提交测试

---
+++ mvn1/src/main/java/com/chen/Test.java
@@ -0,0 +1,16 @@
+package com.chen;
+
+/**
+ * ClassName: Test
+ * Package: com.chen
+ * Description:
+ *
+ * @Author chenxingzhi
+ * @Create 2023/10/9 9:47
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
