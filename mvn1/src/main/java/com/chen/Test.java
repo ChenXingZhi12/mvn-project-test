0e3e4fbc2f0611665aa22566dc80ad8ba61dfbb2
ChenXingZhi12 <909716454@qq.com>
2023-10-09 10:12:28 +0800

    cxz第2次提交测试

--- mvn1/src/main/java/com/chen/Test.java
+++ mvn1/src/main/java/com/chen/Test.java
@@ -12,8 +12,9 @@ package com.chen;
 public class Test {
     public static void main(String[] args) {
 
+        System.out.println("第一次提交");
         System.out.println("第二次提交");
-        System.out.println("第二次提交");
+        System.out.println("第三次提交");
 
     }
 }
